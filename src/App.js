c6fc6351049c7edcb7a761b4c6892212bab4c450
khalid-zein <zeinkhalid004@gmail.com>
2023-03-07 13:29:46 +0300

    added hero section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { BrowserRouter} from 'react-router-dom';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import './App.css';
 import React, {useState} from 'react';
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(!!JSON.parse(localStorage.getItem('loggedIn')))
@@ -10,6 +11,9 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Navbar loggedIn={loggedIn} setLoggedIn={setLoggedIn}/>
+        <Routes>
+          <Route path='/' element={ <Home /> } />
+        </Routes>
       </BrowserRouter>
     </div>
   );
